ec6e7b2abef977b15fdda0ad549e535174d84caa
phamhuudat <phamhuudatk58@gmail.com>
2021-07-31 22:35:44 +0700

    Initial check-in of module eShopSolution

---
+++ eShopSolution.WebAPp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace eShopSolution.WebAPp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
